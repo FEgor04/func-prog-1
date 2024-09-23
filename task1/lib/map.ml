71766df2eb787fdb397329927ba215a9c73c5922
Egor Fedorov <fegor2004@gmail.com>
2024-09-23 22:07:32 +0300

    dune fmt

--- task1/lib/map.ml
+++ task1/lib/map.ml
@@ -2,9 +2,8 @@ let is_good x = x mod 3 == 0 || x mod 5 == 0
 let generate_sequence n = List.init n (fun x -> x + 1)
 let map_good x = if is_good x then x else 0
 
-let sum_3_5 n = generate_sequence n
-|> List.map map_good
-|> List.fold_left (+) 0
+let sum_3_5 n =
+  generate_sequence n |> List.map map_good |> List.fold_left ( + ) 0
 
 let%test _ = sum_3_5 9 = 23
 let%test _ = sum_3_5 999 = 233168
